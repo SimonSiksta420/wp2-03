98ef03c69966968811112878ff5b547331c3fb3d
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-22 15:29:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$converter = filter_input(INPUT_TYPE, 'money')
+$converter = filter_input(INPUT_POST, 'money')
+$submit = filter_input(INPUT_POST, 'submit');
 ?>
 
 <!DOCTYPE html>
@@ -11,11 +12,25 @@ $converter = filter_input(INPUT_TYPE, 'money')
 </head>
 <body>
     Zadejte peníze v Kč <input type="text" name="money">
-    <br> <input type="submit" name="submit" value="Odeslat">
-    <?php
+    <p> <input type="submit" name="submit" value="Odeslat"> </p>
 
+    <?php
+ if (isset($submit)) { ?>
+ Peníze v eurech: <?= $converter ?>
+
+ <?php
+  if ($converter = $converter * 27) { ?>
+      Peníze v eurech
+  <?php    
+  } else { } ?>
+ 
+ 
+ 
+ 
+<?php
+} else { } ?>
  
 
-    ?>
+
 </body>
 </html>
