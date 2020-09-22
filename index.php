85f57cf6e48e410efc0f1dd09e34b180be6c58c1
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-22 15:13:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,21 @@
 <?php
- $submit = filter_input(INPUT_POST, 'submit');
- $spz = filter_input(INPUT_POST, 'spz');
- $auto = filter_input(INPUT_POST, 'auto');
- $stariauta = filter_input(INPUT_POST, 'stariauta');
- $types = array('Osobní automobil', 'Autobus','Dodávka','Nákladní automobil');
- ?>
+$converter = filter_input(INPUT_TYPE, 'money')
+?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title> Prodej aut </title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Converter </title>
 </head>
 <body>
+    Zadejte peníze v Kč <input type="text" name="money">
+    <br> <input type="submit" name="submit" value="Odeslat">
+    <?php
 
-<?php
-if (isset($submit)) { ?>
-  <p> Vozidlo: <?= $auto ?> </p>
-  <p> SPZ: <?= $spz ?> </p>
-  <p> Stáří auta: <?= $stariauta ?> </p>
-  
- <?php
-  if ($stariauta > 2009) { ?>
-    Moderní
-  <?php
-  } elseif ($stariauta > 1989) { ?>
-    Ojeté
-  <?php
-  } else { ?>
-    Veterán
-  <?php
-  }
+ 
 
-
-
-
-
-} else {  ?>
-
-<form action="index.php" method="post">
-
- Rok výroby: <input type="number" id="stari" name="stariauta" min="1900" max="2020">
-
- Typ vozidla: <select name="auto" id="cars">
- <?php foreach ($types as $type) { ?>
-   <option value = <?= $type ?>"><?= $type ?> </option>
- <?php } ?>
-
-</select>
-<br> SPZ <input type="text" name="spz" > 
-<br> <input type="submit" name="submit" value="Odeslat">
-</form>
-<?php } ?>
-</form>
+    ?>
 </body>
 </html>
