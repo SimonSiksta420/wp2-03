2d3539d6cbcc0f9c03ee543b8e6d97ee58f142e0
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-09-22 16:53:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
-
+$submit = filter_input(INPUT_POST, 'submit');
+$czkeuro = filter_input(INPUT_POST, 'czkdoeuro');
+$eurodoczk = filter_input(INPUT_POST, 'eurodoczk');
+$kurzeuro = 27;
+$kurzczk = 0,37;
+$kolikpenez = filter_input(INPUT_POST, 'penize')
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cz">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Penežní converter </title>
 </head>
+
+<?php
+
+if(isset($submit)) { ?>
+
+
+} 
 
 <body>
     
